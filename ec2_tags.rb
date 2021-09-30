9ee0fe1d844824f1b9ac27ea14e1b66c59d765c0
Wilton Ferreira <wilton.ferreira@gmail.com>
2021-09-30 17:53:53 -0300

    Listagem, Start e Stop
    Adicionei os parâmetros de linha de comando para iniciar (START) e (STOP) para finalizar.

--- ec2_tags.rb
+++ ec2_tags.rb
@@ -9,7 +9,7 @@ minhatag = 'ScheduledStartStop'
 #Metodo para iniciar a instancia
 def instance_started?(ec2_client, instance_id)
     response = ec2_client.describe_instance_status(instance_ids: [instance_id])
-  
+
     if response.instance_statuses.count.positive?
         state = response.instance_statuses[0].instance_state.name
         case state
@@ -25,12 +25,12 @@ def instance_started?(ec2_client, instance_id)
             return false
         end
     end
-  
+
     ec2_client.start_instances(instance_ids: [instance_id])
     ec2_client.wait_until(:instance_running, instance_ids: [instance_id])
         puts 'Instance started.'
     return true
-  
+
     rescue StandardError => e
         puts "Error starting instance: #{e.message}"
     return false
@@ -39,7 +39,7 @@ end
 
 def instance_stopped?(ec2_client, instance_id)
     response = ec2_client.describe_instance_status(instance_ids: [instance_id])
-  
+
     if response.instance_statuses.count.positive?
         state = response.instance_statuses[0].instance_state.name
         case state
@@ -55,12 +55,12 @@ def instance_stopped?(ec2_client, instance_id)
                 return false
         end
     end
-  
+
     ec2_client.stop_instances(instance_ids: [instance_id])
     ec2_client.wait_until(:instance_stopped, instance_ids: [instance_id])
     puts 'Instance stopped.'
     return true
-  
+
     rescue StandardError => e
         puts "Error stopping instance: #{e.message}"
         return false
@@ -82,8 +82,8 @@ def lista_instancekeys(ec2_resource,minhatag,minhakey)
             print "#{instance.id}, #{instance.state.name}"
             instance.tags.each do |tag|
                 print ", #{tag.key}/#{tag.value}"
-                @my_instances.push(instance.id)
             end
+            @my_instances.push(instance.id)
             print "\n"
         end
     end
@@ -98,19 +98,39 @@ client= Aws::EC2::Client.new(region: region)
 resource = Aws::EC2::Resource.new(region: region)
 
 puts "\r\n"
-puts 'LISTAS DAS INSTANCIAS COM TAG PARA DESLIGAMENTO'
+puts 'LISTAS DAS INSTANCIAS COM TAG PARA SCHEDULING'
 puts '-' *47
 lista_instancekeys(resource,minhatag,minhakey)
 puts '-' *19
 puts "\r\n"
 
-#LISTA DAS INSTANCIAS
-#puts @my_instances.uniq
+#Lista das instancias com tag
+#@my_instances
+
 
 #unless instance_stopped?(client, instance_id)
 #    puts 'Could not stop instance.'
 #end
 
-#unless instance_started?(client, instance_id)
-#    puts 'Could not start instance.'
-#end
+if ARGV[0] == 'start'
+    puts "\r\n starting... \r\n"
+    #unless instance_started?(client, instance_id)
+    #    puts 'Could not start instance.'
+    #end
+    @my_instances.each do |machine|
+        unless instance_started?(client,machine)
+            puts machine + " não pode ser iniciada!"
+        end
+    end
+end
+if ARGV[0] == 'stop'
+    puts "\r\n stoping... \r\n"
+    #unless instance_started?(client, instance_id)
+    #    puts 'Could not start instance.'
+    #end
+    @my_instances.each do |machine|
+        unless instance_stopped?(client,machine)
+            puts machine + " não pode ser desligada!"
+        end
+    end
+end
